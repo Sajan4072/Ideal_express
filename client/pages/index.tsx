18bd1ad9a01d893dec6b9b355066fac687106690
jit rana <jitbdrrana8@gmail.com>
2022-05-23 21:53:49 +0545

    feat(ideal courier):initialization of project @nextjs @typescript @tailwindcss

---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className={"styles.container"}>
+       <div className='container bg-black text-white'>
+         container
+       </div>
+    </div>
+  )
+}
+
+export default Home
